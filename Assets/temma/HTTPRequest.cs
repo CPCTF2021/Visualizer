34add4bfbca8d2afcbc8fae7bac6f0a6f2efd0c6
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-02-19 19:57:32 +0900

    :seedling:HTTP Request & WebSocket

---
+++ Assets/temma/HTTPRequest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HTTPRequest : MonoBehaviour
+{
+    public GameObject text_object = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetRequest("https://www.example.com"));
+        StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                this.GetComponent<Text>().text = webRequest.downloadHandler.text;
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
---
+++ Assets/temma/WebsocketExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebsocketExample : MonoBehaviour
+{
+    WebSocket ws;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ws = new WebSocket ("ws://localhost:3000"); // $ npx wscat -l 3000
+        ws.EmitOnPing = true;
+        ws.OnMessage += (sender, e) =>
+            Debug.Log("Laputa says: " + e.Data);
+
+        ws.Connect();
+        ws.Send("BALUS");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+}
