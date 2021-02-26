0d43b6ec1cd52bd0248567ef735b5df0f0de4ff8
ntaso2051 <normalforce1227@gmail.com>
2021-02-26 16:42:10 +0900

    uoooooooooooooo

---
+++ Assets/nworkspace/Scripts/Ellipse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ellipse : MonoBehaviour
+{
+    const float radius1 = 5.0f;
+    const float radius2 = 8.0f;
+    public float time = 0.0f;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        Vector3 tmpPos = this.transform.position;
+        tmpPos.x = radius1 * Mathf.Cos(2 * Mathf.PI * time);
+        tmpPos.y = radius2 * Mathf.Sin(2 * Mathf.PI * time);
+        this.transform.position = tmpPos;
+    }
+}
---
+++ Assets/nworkspace/Scripts/IconGen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconGen : MonoBehaviour
+{
+    int iconNum=10;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
