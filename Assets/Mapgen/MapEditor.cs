11381e2003d5066e980a94326784b796ca0722c4
fogrexon <fogrexon@gmail.com>
2021-04-03 22:51:49 +0900

    途中

---
+++ Assets/Mapgen/MapEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Map;
+
+namespace Map
+{
+    [CustomEditor(typeof(MapGeneratorV2))]
+    public class MapEditor : Editor
+    {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapGeneratorV2 generator = GetComponent<MapGeneratorV2>();
+    }
+
+    }
+}
---
+++ Assets/Mapgen/MapGeneratorV2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map
+{
+    public class MapGeneratorV2: MonoBehaviour
+    {
+        [SerializeField]
+        ComputerShader meshBuilder;
+        public void BuildMesh()
+        {
+
+        }
+    }
+}
