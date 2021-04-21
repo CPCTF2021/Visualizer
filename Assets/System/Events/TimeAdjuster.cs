bf02c9fab75ed061950ce371826d48259f611226
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-04-21 10:22:45 +0900

    =:zap:TimerAdjuster

--- Assets/System/Events/TimeAdjuster.cs
+++ Assets/System/Events/TimeAdjuster.cs
@@ -7,22 +7,33 @@ namespace VisualizerSystem
     public class TimeAdjusterEvent
     {
         static UserManager userManager;
-        public TimeAdjusterEvent(UserManager manager)
+        Timer timer;
+        int counter = 0;
+        public TimeAdjusterEvent(UserManager manager, Timer timer)
         {
             userManager = manager;
+            this.timer = timer;
         }
         [Serializable]
         private class EventDetail
         {
-            public int timeLimit;
+            public float timeLimit;
         }
         public void Handler(EventType type, string msg)
         {
-            //    Event<EventDetail> e = JsonUtility.FromJson<Event<EventDetail>>(args.Data);
-            //    if (e.type == EventType.TimeAdjuster)
-            //    {
-
-            //    }
+            if (type == EventType.TimeAdjuster)
+            {
+                // {"type":0,"data":{"timeLimit": 3000}
+                Event<EventDetail> e = JsonUtility.FromJson<Event<EventDetail>>(msg);
+                timer.limitTime = e.data.timeLimit;
+                if (counter == 5)
+                {
+                    counter = 0;
+                    return;
+                }
+                counter++;
+                return;
+            }
         }
     }
 }
--- Assets/System/SystemInitializer.cs
+++ Assets/System/SystemInitializer.cs
@@ -23,13 +23,16 @@ namespace VisualizerSystem
             userManager.Initialize();
             rankingManager = GameObject.Find("RankingPanel").GetComponent<RankingManager>();
 
-            #if !UNITY_EDITOR
+#if !UNITY_EDITOR
 
             // Sync();
 
+            rankingManager = GameObject.Find("RankingPanel").GetComponent<RankingManager>();
+            timer = GameObject.Find("Timer").GetComponent<Timer>();
+
             eventManager.Init();
 
-            TimeAdjusterEvent timeAdjusterEvent = new TimeAdjusterEvent(userManager);
+            TimeAdjusterEvent timeAdjusterEvent = new TimeAdjusterEvent(userManager, timer);
             eventManager.Register(timeAdjusterEvent.Handler);
 
             UserCreatedEvent userCreatedEvent = new UserCreatedEvent(userManager, rankingManager);
@@ -38,19 +41,19 @@ namespace VisualizerSystem
             ProblemSolvedEvent problemSolvedEvent = new ProblemSolvedEvent(userManager);
             eventManager.Register(problemSolvedEvent.Handler);
 
-            #endif
+#endif
         }
         void Update()
         {
-            #if !UNITY_EDITOR
+#if !UNITY_EDITOR
             eventManager.Handle();
-            #endif
+#endif
         }
         void OnDestroy()
         {
-            #if !UNITY_EDITOR
+#if !UNITY_EDITOR
             eventManager.Shutdown();
-            #endif
+#endif
         }
         [Serializable]
         public class UserResponse
