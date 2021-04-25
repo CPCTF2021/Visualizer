38d1f0c107a4b4c1fd54bd99181055db6bbde80a
fogrexon <fogrexon@gmail.com>
2021-04-25 12:54:06 +0900

    api

--- Assets/System/SystemInitializer.cs
+++ Assets/System/SystemInitializer.cs
@@ -24,11 +24,11 @@ namespace VisualizerSystem
             // GetComponent<TreeGenerator>().MakeTree();
             userManager = GetComponent<UserManager>();
             userManager.Initialize();
-#if !UNITY_EDITOR
             Sync();
-
             rankingManager = GameObject.Find("RankingPanel").GetComponent<RankingManager>();
             timer = GameObject.Find("Timer").GetComponent<Timer>();
+            
+#if !UNITY_EDITOR
 
             eventManager.Init();
 
@@ -93,31 +93,55 @@ namespace VisualizerSystem
             try { rankingManager.AddUsers(userManager.users.Where(user => user.id != "").ToList()); }
             catch (ArgumentException err) { Debug.LogError(err); }
         }
+        // async void Sync()
+        // {
+        //     List<UserResponse> res = await FetchUsers();
+        //     List<User> users = new List<User>(res.Count);
+        //     var tasks = new List<Task<Texture2D>>();
+        //     var semaphore = new SemaphoreSlim(30, 30);
+        //     foreach (UserResponse user in res)
+        //     {
+        //         await semaphore.WaitAsync();
+        //         tasks.Add(FetchIcon(user.iconUrl));
+        //         semaphore.Release(1);
+        //     }
+        //     var icons = await Task.WhenAll(tasks);
+        //     for (int i = 0; i < users.Count; i++)
+        //     {
+        //         var scores = new Dictionary<Genre, float>();
+        //         foreach (Score score in res[i].scores)
+        //         {
+        //             scores.Add(score.genre, score.score);
+        //         }
+        //         var tmp = new User();
+        //         Texture2D texture = new Texture2D(5, 5);
+        //         tmp.name = res[i].name;
+        //         tmp.id = res[i].id;
+        //         tmp.icon = icons[i];
+        //         tmp.scores = scores;
+        //         users.Add(tmp);
+        //     }
+        //     try { userManager.AddUsers(users); }
+        //     catch (MissingFieldException err) { Debug.LogError(err); }
+        //     try { rankingManager.AddUsers(userManager.users.Where(user => user.id != "").ToList()); }
+        //     catch (ArgumentException err) { Debug.LogError(err); }
+        // }
         async void Sync()
         {
             List<UserResponse> res = await FetchUsers();
             List<User> users = new List<User>(res.Count);
-            var tasks = new List<Task<Texture2D>>();
-            var semaphore = new SemaphoreSlim(30, 30);
             foreach (UserResponse user in res)
             {
-                await semaphore.WaitAsync();
-                tasks.Add(FetchIcon(user.iconUrl));
-                semaphore.Release(1);
-            }
-            var icons = await Task.WhenAll(tasks);
-            for (int i = 0; i < users.Count; i++)
-            {
                 var scores = new Dictionary<Genre, float>();
-                foreach (Score score in res[i].scores)
+                foreach (Score score in user.scores)
                 {
                     scores.Add(score.genre, score.score);
                 }
                 var tmp = new User();
                 Texture2D texture = new Texture2D(5, 5);
-                tmp.name = res[i].name;
-                tmp.id = res[i].id;
-                tmp.icon = icons[i];
+                tmp.name = user.name;
+                tmp.id = user.id;
+                tmp.icon = await FetchIcon(user.iconUrl);
                 tmp.scores = scores;
                 users.Add(tmp);
             }
